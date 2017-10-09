2a291541b587440e02c889fd0468cf343937ecaf
Gil Alexandel <gilalexander11@gmail.com>
2017-10-09 10:25:09 -0400

    Testing json file
    Added json file
    Fixed jQuery reference

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     var url  = window.location.href;
     var title = $( "#author" ).text()
 
-    
+   
     $("#btnTweet").click(function() {
         window.open("https://twitter.com/intent/tweet?text=" + "Biografía de " +title + " "+url);
       });
@@ -11,5 +11,24 @@ $( document ).ready(function() {
        /*  window.open("http://www.facebook.com/share.php?u=[URL]&title=[TITLE]"); */
         window.open("http://www.facebook.com/share.php?u=https://gilalexandel.github.io/PersonajesDominicanos/pages/liborio.html");
       });
+
+    //Function to get bio from json file
+    function getbio(){
+
+      var bio = "../content/bio.json";
+
+      $.getJSON(bio, function(data){
+
+        //Addung
+        $( "#content" ).append( "<h4> <strong"+data.firstName+"</strong></h4>" );
+
+
+
+      });
+
+      
+
+    }
+    getbio();
     
 });
